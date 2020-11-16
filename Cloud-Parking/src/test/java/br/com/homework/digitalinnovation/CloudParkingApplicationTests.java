86d4e823e8afd08cf31599f8f71043fe3ed5ea92
Bill <billmetal9@gmail.com>
2020-11-16 20:33:51 -0200

    Deply inicial

---
+++ Cloud-Parking/src/test/java/br/com/homework/digitalinnovation/CloudParkingApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.homework.digitalinnovation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudParkingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
